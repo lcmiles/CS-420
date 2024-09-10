3de3545a358887fd59c2da972c3cf5122e3bd344
lmiles1511 <102430758+lmiles1511@users.noreply.github.com>
2024-09-10 11:12:27 -0500

    unwrapped vbox fxml

--- Assignment1/src/main/java/cs420/assignment1/app.java
+++ Assignment1/src/main/java/cs420/assignment1/app.java
@@ -10,24 +10,18 @@ public class app extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Load the FXML file
         Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
 
-        // Create the scene with the loaded FXML layout
         Scene scene = new Scene(root);
 
-        // Set the title of the window (Stage)
         primaryStage.setTitle("Dashboard");
 
-        // Set the scene on the stage
         primaryStage.setScene(scene);
 
-        // Show the stage
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        // Launch the application
         launch(args);
     }
 }

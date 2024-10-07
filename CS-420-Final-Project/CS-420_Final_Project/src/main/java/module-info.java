14040b67d7464b8f669cca0218e0a2418937a18e
Logan Miles <144288661+lcmiles@users.noreply.github.com>
2024-10-07 15:09:55 -0500

    Delete CS-420-Final-Project directory

--- CS-420-Final-Project/CS-420_Final_Project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cs420.cs420_final_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens cs420.cs420_final_project to javafx.fxml;
-    exports cs420.cs420_final_project;
-}
